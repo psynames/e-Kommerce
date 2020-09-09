17c33a77932a799af147995582b417bb418e7bf6
psyname <48337480+psynames@users.noreply.github.com>
2020-09-09 17:42:24 +0200

    configured Database connection

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CORE.Entities;
+using INFRASTUCTURE.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreDbContext _dbcontext;
+        public ProductsController(StoreDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Product>> GetProducts()
+        {
+            var products =await _dbcontext.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _dbcontext.Products.FindAsync(id);
+            return Ok(product);
+        }
+    }
+}
