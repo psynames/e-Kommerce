6fa22d23a122dfd5a7c59b0d56152a3952025fab
psyname <48337480+psynames@users.noreply.github.com>
2020-09-15 15:48:20 +0200

    configured the CORS

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,6 +36,13 @@ namespace API
 
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200;");
+                });
+            });
          
         }
 
@@ -54,6 +61,8 @@ namespace API
             app.UseRouting();
 
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
